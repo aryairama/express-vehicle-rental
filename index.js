d927480a31026050e353ff1795effb6ca533d919
Arya Irama Wahono <aryairama987@gmail.com>
2021-08-16 20:11:50 +0700

    initial project

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import 'dotenv/config';
+import cors from 'cors';
+import fileUpload from 'express-fileupload';
+
+const app = express();
+const port = process.env.PORT_APPLICATION;
+app.use('/public', express.static(path.resolve('./public')));
+app.use(fileUpload());
+app.use(cors());
+app.use(express.json());
+app.use('*', (req, res, next) => {
+  next(new Error('Endpoint Not Found'));
+});
+// app.use((err, req, res, next) => {
+//   helpers.responseError(res, 'Error', 500, err.message, []);
+// });
+app.listen(port, () => {
+  console.log(`server running port ${port}`);
+});
