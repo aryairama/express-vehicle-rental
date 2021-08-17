c991ae77347ad9f0dc76ebcc19bbb58a53425b06
Arya Irama Wahono <aryairama987@gmail.com>
2021-08-17 21:06:42 +0700

    start backend

---
+++ src/helpers/jwt.js
@@ -0,0 +1,38 @@
+/* eslint-disable import/prefer-default-export */
+import Jwt from 'jsonwebtoken';
+import { redis } from '../middlewares/Redis.js';
+
+export const genAccessToken = (payload, option) =>
+  new Promise((resolve, reject) => {
+    Jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { ...option }, (err, token) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      resolve(token);
+    });
+  });
+
+export const genRefreshToken = (payload, option) =>
+  new Promise((resolve, reject) => {
+    Jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { ...option }, (err, token) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      redis.set(`jwtRefToken-${payload.user_id}`, token, 'EX', option.expiresIn);
+      resolve(token);
+    });
+  });
+
+export const genVerifEmailToken = (payload, option) =>
+  new Promise((resolve, reject) => {
+    Jwt.sign(payload, process.env.VERIF_EMAIL_TOKEN_SECRET, { ...option }, (err, token) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      redis.set(`jwtEmailVerToken-${payload.user_id}`, token, 'EX', option.expiresIn);
+      resolve(token);
+    });
+  });
