765473ed4c59968d64dfead5c2ac91a313c9784f
Arya Irama Wahono <aryairama987@gmail.com>
2021-09-20 21:25:11 +0700

    enable cookie detail reservation

--- src/routes/reservations.js
+++ src/routes/reservations.js
@@ -9,7 +9,6 @@ router
   .get('/', Auth, Role('user', 'admin'), ValidationReservations('read'), ControllerReservations.history)
   .post('/', Auth, Role('user'), ValidationReservations('create'), ControllerReservations.addReservation)
   .patch('/:id', Auth, Role('user', 'admin'), ValidationReservations('update'), ControllerReservations.updateReservation)
-  .get('/:id', ValidationReservations('detail'), ControllerReservations.detailReservation);
-  // .get('/:id', Auth, Role('user', 'admin'), ValidationReservations('detail'), ControllerReservations.detailReservation);
+  .get('/:id', Auth, Role('user', 'admin'), ValidationReservations('detail'), ControllerReservations.detailReservation);
 
 module.exports = router;
