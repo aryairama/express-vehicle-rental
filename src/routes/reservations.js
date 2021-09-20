f9c51ae091f3cba1c07a99d88fdd6435cb62daca
Arya Irama Wahono <aryairama987@gmail.com>
2021-09-20 21:09:47 +0700

    disable cookie detail reservation

--- src/routes/reservations.js
+++ src/routes/reservations.js
@@ -9,6 +9,7 @@ router
   .get('/', Auth, Role('user', 'admin'), ValidationReservations('read'), ControllerReservations.history)
   .post('/', Auth, Role('user'), ValidationReservations('create'), ControllerReservations.addReservation)
   .patch('/:id', Auth, Role('user', 'admin'), ValidationReservations('update'), ControllerReservations.updateReservation)
-  .get('/:id', Auth, Role('user', 'admin'), ValidationReservations('detail'), ControllerReservations.detailReservation);
+  .get('/:id', ValidationReservations('detail'), ControllerReservations.detailReservation);
+  // .get('/:id', Auth, Role('user', 'admin'), ValidationReservations('detail'), ControllerReservations.detailReservation);
 
 module.exports = router;
